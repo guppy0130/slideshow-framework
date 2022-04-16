fe333eb386ad16cd53341421af31939685fda52a
guppy0130 <guppy0130@yahoo.com>
2022-04-15 22:01:15 -0500

    presentation mode, timer, UI

--- README.md
+++ README.md
@@ -7,23 +7,12 @@
     contains the markdown to parse
   * example is [template.html](template.html)
   * include markdown-it and, optionally, highlight.js
-* Build the TS and include in project
-* Include this in a `<script>` tag to start the presentation
-
-  ```javascript
-  var slideshow = create_slideshow({
-    // what should separate slides? default is `\n---\n`
-    slide_separator: "\n---\n",
-    // you can pass in a custom markdown-it renderer here, and we'll just call
-    // md.render() with your passed-in instance. This allows you to configure
-    // your markdown-it plugins, your hljs configuration, etc.
-    markdown_renderer: false,
-    // keybindings to navigate between slides
-    keys: {}
-  });
-  ```
+* Build the content in `src` and include in project
+* Call `create_slideshow()` to start the presentation
 
 ## Usage
+
+### Keybindings
 
 The following keybindings come pre-configured:
 
@@ -53,3 +42,39 @@ document.addEventListener("keydown", (e) => {
   }
 });
 ```
+
+### Presenter Mode
+
+Use `p` to access presenter's mode. In presenter's mode, a panel on the right
+displays the presentation runtime and presenter notes for the current slide.
+
+In the markdown, presenter notes for a slide come after the slide and are
+delineated with `???` surrounded by newlines. After slides are parsed, the first
+sequence of `\n???\n` will be considered the presenter notes marker, and all
+content between that marker and the next slide will be rendered as presenter
+notes. The presenter notes section supports markdown.
+
+### CSS
+
+`src/*.scss` should be compiled to `dest/*.css`. Include `index.scss` at the
+minimum; this provides the logic for rendering/hiding slide elements. It also
+provides logic for printing slides.
+
+Your template can include its own theme; to get started, you can modify CSS for
+the following classes:
+
+* `.slide`
+* `.presenterNotes`
+* `.timer-container`
+
+The `hidden` HTML property will be on slides and presenter notes that are not
+active.
+
+### Printing
+
+The CSS in `index.scss` is configured so that each slide will print on its own
+page. This makes it easy for folks to print N-many on the same page via their
+printing options.
+
+The extras panel on the right in presenter's mode will not be visible during
+printing. You cannot print the presenter's notes at this time.
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,42 @@
 // wrap this all up inside a function so we can call it when we want to from
 // the parent
-const create_slideshow = (options: {
-  slide_separator: string;
-  markdown_renderer: markdownit;
-  keys: {};
+const createSlideshow = (options: {
+  slideSeparator: string;
+  markdownRenderer: markdownit;
 }) => {
-  // fetch content
-  const slide_separator = options["slide_separator"] || "\n---\n";
+  // slideshow properties
+  let slideIndex = 0;
+  let extrasVisible = true;
 
-  let content_elem = document.getElementById("content");
-  content_elem.style.display = "none";
-  let content_arr = content_elem.innerText.split(slide_separator);
+  // fetch content
+  const slideSeparator = options["slideSeparator"] || "\n---\n";
+  const presenterNotesSeparator = "\n???\n";
+
+  // hide the original markdown content
+  let rawContentElement = document.getElementById("content");
+
+  // split the content into an array of markdown chunks. each chunk is a slide.
+  let rawContentArray = rawContentElement.innerText.split(slideSeparator);
+
+  // extract presenter notes from slides
+  // the assumption is that presenter notes will follow the slide
+  let slideshowData: {
+    slide: string;
+    presenterNotes: string;
+  }[] = [];
+  for (const content of rawContentArray) {
+    let [trueContent, presenterNotes] = content.split(
+      presenterNotesSeparator,
+      2
+    );
+    slideshowData.push({
+      slide: trueContent.trim(),
+      presenterNotes: presenterNotes?.trim(),
+    });
+  }
 
   // initialize markdown renderer
-  let default_markdownit_options = {
+  let defaultMarkdownitOptions = {
     highlight: (str: string, lang: string) => {
       if (lang && hljs.getLanguage(lang)) {
         return hljs.highlightAuto(str, [lang]).value;
@@ -25,63 +48,185 @@ const create_slideshow = (options: {
   };
   let md: markdownit;
   if (
-    options.markdown_renderer &&
-    options.markdown_renderer instanceof window.markdownit
+    options.markdownRenderer &&
+    options.markdownRenderer instanceof window.markdownit
   ) {
-    md = options.markdown_renderer;
+    md = options.markdownRenderer;
   } else {
-    md = window.markdownit(default_markdownit_options);
+    md = window.markdownit(defaultMarkdownitOptions);
   }
 
   // initialize slideshow container
-  let slideshow_container = document.createElement("div");
-  slideshow_container.classList.add("slideshow-container");
+  let slideshowContainer = document.createElement("div");
+  slideshowContainer.classList.add("slideshow-container");
 
-  // add slides to the container
-  let slides: HTMLDivElement[] = [];
-  for (const c of content_arr) {
-    let result = md.render(c.trim());
-    let slide = document.createElement("div");
-    slide.classList.add("slide");
-    slide.innerHTML = result;
-    slide.style.display = "none";
-    slideshow_container.appendChild(slide);
-    slides.push(slide);
+  // initialize extras container (timer, presenter notes)
+  let extrasContainer = document.createElement("div");
+  extrasContainer.classList.add("extras-container");
+
+  // initialize timer
+  let timerInterval: NodeJS.Timer;
+  let slideshowTimer = 0;
+  function renderTimer() {
+    const h = Math.floor(slideshowTimer / 3600);
+    const m = Math.floor((slideshowTimer % 3600) / 60);
+    const s = slideshowTimer % 60;
+    let timerString = [
+      h,
+      m > 9 ? m : h ? "0" + m : m || "00",
+      s > 9 ? s : "0" + s || "00",
+    ]
+      .filter(Boolean)
+      .join(":");
+    timerPElement.innerText = timerString;
+  }
+  let tryStartTimer = () => {
+    if (extrasVisible && !timerInterval) {
+      timerInterval = setInterval(() => {
+        ++slideshowTimer;
+        renderTimer();
+      }, 1000);
+    }
+  };
+  let stopTimer = () => {
+    if (timerInterval) {
+      clearInterval(timerInterval);
+      timerInterval = null;
+      pauseStartTimerButton.innerText = "Start";
+    }
+  };
+  let startTimer = () => {
+    if (!timerInterval) {
+      tryStartTimer();
+      pauseStartTimerButton.innerText = "Pause";
+    }
+  };
+  let resetTimer = () => {
+    stopTimer();
+    slideshowTimer = 0;
+    renderTimer();
+  };
+
+  // timer elements (text, buttons)
+  let timerContainer = document.createElement("section");
+  timerContainer.classList.add("timer-container");
+
+  // for timer text
+  let timerTitle = document.createElement("h2");
+  timerTitle.innerText = "Timer";
+  timerContainer.appendChild(timerTitle);
+  let timerPElement = document.createElement("p");
+  timerContainer.appendChild(timerPElement);
+
+  // controls
+  let timerButtonContainer = document.createElement("div");
+  timerButtonContainer.id = "timer-button-container";
+  timerContainer.appendChild(timerButtonContainer);
+
+  // reset to zero + stop
+  let resetTimerButton = document.createElement("button");
+  resetTimerButton.id = "reset-timer-button";
+  resetTimerButton.innerText = "Reset";
+  resetTimerButton.addEventListener("click", resetTimer);
+  timerButtonContainer.appendChild(resetTimerButton);
+
+  // pause/start
+  let pauseStartTimerButton = document.createElement("button");
+  pauseStartTimerButton.id = "pause-restart-timer-button";
+  // initialize the button
+  pauseStartTimerButton.addEventListener("click", () => {
+    timerInterval ? stopTimer() : startTimer();
+  });
+  pauseStartTimerButton.innerText = timerInterval ? "Pause" : "Start";
+  timerButtonContainer.appendChild(pauseStartTimerButton);
+
+  renderTimer();
+  extrasContainer.append(timerContainer);
+
+  // initialize presenter notes container
+  let presenterNotesContainer = document.createElement("section");
+  presenterNotesContainer.classList.add("presenter-container");
+  let presenterNotesTitle = document.createElement("h2");
+  presenterNotesTitle.innerText = "Presenter Notes";
+  presenterNotesContainer.appendChild(presenterNotesTitle);
+  extrasContainer.appendChild(presenterNotesContainer);
+
+  // add content container to the document
+  let contentContainer = document.createElement("main");
+  contentContainer.id = "content-container";
+  // place it at the end of the document
+  rawContentElement.parentNode.appendChild(contentContainer);
+  contentContainer.appendChild(slideshowContainer);
+  contentContainer.appendChild(extrasContainer);
+
+  // add slides and presentation notes to their containers
+  let renderedSlideElements: {
+    slide: HTMLElement;
+    presenterNotes: HTMLElement;
+  }[] = [];
+  for (const c of slideshowData) {
+    let slideElement: {
+      slide: HTMLElement;
+      presenterNotes: HTMLElement;
+    } = {
+      slide: document.createElement("article"),
+      presenterNotes: document.createElement("aside"),
+    };
+    for (const key in c) {
+      let renderedContent = "";
+      if (c[key]) {
+        renderedContent = md.render(c[key]);
+      }
+      let slide = slideElement[key];
+      slide.innerHTML = renderedContent;
+      slide.classList.add(key);
+      slide.hidden = true;
+    }
+    renderedSlideElements.push(slideElement);
+    slideshowContainer.appendChild(slideElement.slide);
+    presenterNotesContainer.appendChild(slideElement.presenterNotes);
   }
 
-  // add slideshow container to the document
-  content_elem.parentNode.appendChild(slideshow_container);
-
-  // let us move between slides
-  let slideIndex = 0;
-  let showSlide = (index: number) => {
+  let renderSlideAndPresenterNotes = (index: number) => {
     // bound the input so you don't index out of bounds
-    if (index >= slides.length) {
-      index = slides.length - 1;
+    if (index >= renderedSlideElements.length) {
+      index = renderedSlideElements.length - 1;
     }
     if (index < 0) {
       index = 0;
     }
-    // hide all the slides
-    for (let slide of slides) {
-      slide.classList.remove("current");
-      slide.style.display = "none";
+    // hide all the slides + presenter notes
+    for (let slide of renderedSlideElements) {
+      for (const key in slide) {
+        slide[key].hidden = true;
+      }
     }
     // and then show the current slide
-    slides[index].classList.add("current");
-    slides[index].style.display = "block";
-    slideIndex = index;
+    for (const key in renderedSlideElements[index]) {
+      if (!extrasVisible && key === "presenterNotes") {
+        continue;
+      }
+      renderedSlideElements[index][key].hidden = false;
+    }
+    return index;
+  };
+
+  let renderUI = (index: number) => {
+    // update the slideIndex to the index of the slide rendered
+    slideIndex = renderSlideAndPresenterNotes(index);
+    // display extras if necessary
+    extrasContainer.hidden = !extrasVisible;
   };
 
   // convenience functions
-  let nextSlide = () => showSlide(++slideIndex);
-  let prevSlide = () => showSlide(--slideIndex);
+  let nextSlide = () => renderUI(++slideIndex);
+  let prevSlide = () => renderUI(--slideIndex);
 
   // show first slide
-  showSlide(slideIndex);
+  renderUI(slideIndex);
 
   // configure keys
-  let keys = options.keys || {
+  let keys = {
     ArrowRight: nextSlide,
     ArrowDown: nextSlide,
     KeyJ: nextSlide,
@@ -91,19 +236,63 @@ const create_slideshow = (options: {
     ArrowUp: prevSlide,
     KeyH: prevSlide,
     KeyK: prevSlide,
+    KeyP: () => {
+      extrasVisible = !extrasVisible;
+      renderUI(slideIndex);
+    },
   };
 
-  document.addEventListener("keydown", (e) => {
-    // e.preventDefault();
-    // general navigation based on the dictionary
-    if (e.code in keys) {
-      keys[e.code]();
+  document.addEventListener(
+    "keydown",
+    (e) => {
+      // do nothing on ctrl down, so that you preserve ctrl+p functionality.
+      // ctrl+p should not trigger toggling extras panel
+      if (e.ctrlKey) {
+        return;
+      }
+      // general navigation based on the dictionary
+      if (e.code in keys) {
+        keys[e.code]();
+      }
+      // use the number keys to get to a slide directly.
+      // the first slide should map to key 1 (not zero-indexed).
+      if (parseInt(e.key)) {
+        slideIndex = parseInt(e.key) - 1;
+        renderUI(slideIndex);
+      }
+    },
+    true
+  );
+
+  // this isn't strictly necessary (the css will handle all of this
+  // functionality), but it's correct semantically (removes `hidden`).
+  // this won't let you print the presenter notes, but that's just a function of
+  // the presenter notes being in a separate container element.
+  // TODO: maybe figure out how to restructure this to allow printing presenter
+  // notes? very low priority.
+  if (window.matchMedia) {
+    let cachedExtrasVisible: boolean = extrasVisible;
+    let printRender = () => {
+      for (const slide of renderedSlideElements) {
+        slide.slide.hidden = false;
+      }
+      cachedExtrasVisible = extrasVisible;
+      extrasVisible = false;
+      extrasContainer.hidden = true;
+    };
+    // if we toggle between print and not print
+    window.matchMedia("print").addEventListener("change", (e) => {
+      if (e.matches) {
+        // we're printing
+        printRender();
+      } else {
+        // not printing. reset.
+        extrasVisible = cachedExtrasVisible;
+        renderUI(slideIndex);
+      }
+    });
+    if (window.matchMedia("print").matches) {
+      printRender();
     }
-    // use the number keys to get to a slide directly.
-    // the first slide should map to key 1 (not zero-indexed).
-    if (parseInt(e.key)) {
-      slideIndex = parseInt(e.key) - 1;
-      showSlide(slideIndex);
-    }
-  });
+  }
 };
